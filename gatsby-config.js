9c0960868069e1c86dbfe46734adc718ec3532d9
japanvora <08me108@gmail.com>
2019-12-20 01:03:48 +0530

    favicon change

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `standalone`,
-        icon: `content/assets/inward-locus.png`,
+        icon: `content/assets/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
