a7bf825ee75899b7431c91af60cadc2a3a3a2fbb
japanvora <08me108@gmail.com>
2019-12-21 16:14:10 +0530

    margin change

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ class Layout extends React.Component {
         <h1
           style={{
             ...scale(1.5),
-            marginBottom: rhythm(1.5),
+            marginBottom: rhythm(1),
             marginTop: 0,
           }}
         >
