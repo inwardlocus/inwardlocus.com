dd3ed008f127c45f8f667a46a1e268432d5a0019
japanvora <08me108@gmail.com>
2020-03-29 17:45:26 +0530

    Tapgs Post Layout Change

--- src/templates/tag-posts.js
+++ src/templates/tag-posts.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const TagPosts = ({ pageContext, data }) => {
   const { tag } = pageContext;
   const { edges, totalCount } = data.allMarkdownRemark;
-  const tagHeader = `${totalCount} read${
-    totalCount === 1 ? "" : "s"
-  } tagged with "${tag}"`;
+
+  const siteTitle = data.site.siteMetadata.title;
 
   return (
-    <Layout title={tagHeader}>
+    <Layout title={siteTitle}>
+      <h2
+        css={`
+          margin: 28px 0;
+          font-family: "Exo";
+        `}
+      >
+        {totalCount} read{totalCount === 1 ? "" : "s"} tagged with "{tag}"
+      </h2>
+
       <ul
         css={`
           margin: 2rem 0;
@@ -34,6 +43,11 @@ export default TagPosts;
 
 export const pageQuery = graphql`
   query($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allMarkdownRemark(
       limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }
